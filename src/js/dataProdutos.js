3759f4d83f2b195d914f4df498f19e6c64cacf78
NicollyAlves <nicollyalvesdasilva12@gmail.com>
2022-05-30 17:00:46 -0400

    lista de produtos

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,66 @@
-const produtos = [
+const data = [
     {
+        objeto: 'produto',
+        nome: 'Morango 5Kg',
+        preco: 7.00,
+        secao: 'Hortifruit',
+        categoria: 'fruta',
+        img: "./src/img/unsplash_IK2ep3jMnHA.png"
+    },
+    {
+        objeto: 'produto',
         nome: 'Banana',
-        preco: 2,
-        secao: 'Hortifruti',
+        preco: 4.00,
+        secao: 'Hortifruit',
         categoria: 'fruta',
-        img: "./src/img/banana.png"
+        img: './src/img/unsplash_0WW38q7lGZA.png'
     },
     {
-        nome: 'Morango',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/morango.png'
+        objeto: 'produto',
+        nome: 'Yorgut de Morango',
+        preco: 3.00,
+        secao: 'Laticínio',
+        categoria: 'leite',
+        img: './src/img/unsplash_lK1Q5RyD6tc.png'
     },
-    {
-        nome: 'Maçã',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/maça.png'
-    },
-    {
+    { 
+        objeto: 'produto',
         nome: 'Pão',
-        preco: 4,
+        preco: 5.00,
         secao: 'Panificadora',
         categoria: 'Pães',
-        img: './src/img/pao.png'
+        img: './src/img/unsplash__0IzI-gEH4w.png'
     },
     {
+        objeto: 'produto',
         nome: 'Leite',
-        preco: 5,
+        preco: 7.00,
         secao: 'Laticínio',
         categoria: 'Leite',
-        img: './src/img/leite.png'
-    }
+        img: './src/img/unsplash_rbJ9BY8bRRY.png'
+    },
+    {
+        objeto: 'produto',
+        nome: 'Bolo',
+        preco: 10.00,
+        secao: 'Panificadora',
+        categoria: 'Pães',
+        img: './src/img/unsplash_vdx5hPQhXFk.png'  
+    },
+    {
+        objeto: 'produto',
+        nome: 'Torta Maça',
+        preco: 4.00,
+        secao: 'Hortifruit',
+        categoria: 'fruta',
+        img: './src/img/unsplash_L1sES_uVrFI.png'  
+    },
+    {
+        objeto: 'produto',
+        nome: 'Maça',
+        preco: 4.00,
+        secao: 'Hortifruit',
+        categoria: 'fruta',
+        img: './src/img/unsplash_wXuzS9xR49M.png'  
+    },
 ]
